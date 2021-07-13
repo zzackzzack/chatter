f52c554c3eb1795ccf3a157833060c53d92461af
wongyoJung <kbb329@gmail.com>
2021-07-13 23:14:51 +0900

    add kakao add channle button

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ $('.interactive-menu-button a').click(function() {
   $(this).toggleClass('active');
 });
 
+
+
 var scroll = new SmoothScroll('a[href*="#"]');
+
 
 
 $('.more-btn').click(function() {
